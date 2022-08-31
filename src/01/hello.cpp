e04bc1c053609e8f974366cc6c30d312746c721e
consolexin <consolexin@gmail.com>
2022-08-31 19:04:50 +0800

    thread apply all bt

--- src/01/hello.cpp
+++ src/01/hello.cpp
@@ -13,3 +13,11 @@ void hello() {
     std::cout << "Hello Concurrent World\n"; 
     std::this_thread::sleep_for(std::chrono::seconds(1000));
 }
+
+// pstree -lp 进程pid
+
+/* 正在运行的程序，查看线程栈帧
+sudo gdb
+attach 进程pid
+thread apply all bt 
+ */

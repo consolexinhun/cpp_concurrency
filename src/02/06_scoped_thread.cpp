5410e3f502d4c1d879afe0b2d3d294f29bb54a7f
consolexin <consolexin@gmail.com>
2022-09-01 14:35:56 +0800

    scoped_thread

---
+++ src/02/06_scoped_thread.cpp
@@ -0,0 +1,46 @@
+#include <thread>
+
+void do_something(int& i) { i++; }
+void oops();
+void do_something_in_current_thread() {}
+
+class scoped_thread {
+    std::thread t;
+public:
+    explicit scoped_thread(std::thread t_) : t(std::move(t_)) {
+        if (!t.joinable()) {
+            throw std::logic_error("no thread");
+        }
+    }
+    ~scoped_thread() {
+        t.join();
+    }
+    scoped_thread(scoped_thread const&)=delete;
+    scoped_thread& operator=(scoped_thread const&)=delete;
+};
+
+struct func {
+    int& i;
+    func(int& i_): i(i_) {}
+    void operator()() {
+        for (unsigned j = 0; j < 1000000; j++) {
+            do_something(i);  // 
+        }
+    }
+};
+
+int main() {
+    oops();
+    return 0;
+}
+
+void oops() {
+    int some_local_state = 0;
+    scoped_thread t(std::thread(func(some_local_state)));
+    do_something_in_current_thread();
+}  
+
+/**
+ * @brief 
+ * 转移线程所有权
+ */

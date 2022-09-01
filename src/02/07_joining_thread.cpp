dc8f76f9bd04efd16a0e62e381ddc91ad5cc3175
consolexin <consolexin@gmail.com>
2022-09-01 15:06:30 +0800

    joining_thread

---
+++ src/02/07_joining_thread.cpp
@@ -0,0 +1,26 @@
+#include "07_joining_thread.h"
+
+void do_something(int& i) { i++; }
+void oops();
+void do_something_in_current_thread() {}
+
+struct func {
+    int& i;
+    func(int& i_): i(i_) {}
+    void operator()() {
+        for (unsigned j = 0; j < 1000000; j++) {
+            do_something(i);  // 
+        }
+    }
+};
+
+int main() {
+    oops();
+    return 0;
+}
+
+void oops() {
+    int some_local_state = 0;
+    joining_thread th(std::thread(func(some_local_state)));
+    do_something_in_current_thread();
+}  
---
+++ src/02/07_joining_thread.h
@@ -0,0 +1,48 @@
+#include <thread>
+
+class joining_thread {
+    std::thread t;
+public:
+    joining_thread() noexcept=default;
+
+    template<typename Callable, typename ...Args>
+    explicit joining_thread(Callable&& func, Args&& ...args):
+        t(std::forward<Callable>(func), std::forward<Args>(args)...) {}
+    explicit joining_thread(std::thread t_) noexcept: t(std::move(t_)) {}
+    joining_thread(joining_thread&& other) noexcept: t(std::move(other.t)) {}
+    joining_thread& operator=(joining_thread&& other) noexcept {
+        if (joinable()) {
+            join();
+        }
+        t = std::move(other.t);
+        return *this;
+    }
+
+    void swap(joining_thread& other) noexcept {
+        t.swap(other.t);
+    } 
+
+    std::thread::id get_id() const noexcept {
+        return t.get_id();
+    }
+
+    bool joinable() const noexcept {
+        return t.joinable();
+    }
+
+    void join() {
+        t.join();
+    }
+
+    void detech() {
+        t.detach();
+    }
+
+    std::thread& as_thread() noexcept {
+        return t;
+    }
+
+    const std::thread& as_thread() const noexcept {
+        return t;
+    }
+};

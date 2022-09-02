4a90024d51281b1f125b91134ef8cb65bdbecea0
consolexin <consolexin@gmail.com>
2022-09-02 16:25:06 +0800

    format

--- src/02/01_detach.cpp
+++ src/02/01_detach.cpp
@@ -1,14 +1,14 @@
 #include <thread>
 
-void do_something(int& i) { i++; }
+void do_something(int* i) { (*i)++; }
 void oops();
 
 struct func {
-    int& i;
-    func(int& i_): i(i_) {}
+    int* i;
+    explicit func(int* i_): i(i_) {}
     void operator()() {
         for (unsigned j = 0; j < 1000000; j++) {
-            do_something(i);  // 
+            do_something(i);  //
         }
     }
 };
@@ -20,10 +20,11 @@ int main() {
 
 void oops() {
     int some_local_state = 0;
-    func my_func(some_local_state);
+    func my_func(&some_local_state);
     std::thread my_thread(my_func);
     my_thread.detach();  // 不等待线程结束
 }  // 分离线程可能还在运行
+
 
 /**
  * @brief 

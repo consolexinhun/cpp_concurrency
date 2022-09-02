0be52b7f6a6debaa0cb40fe79d600671c4c02d09
consolexin <consolexin@gmail.com>
2022-09-02 16:30:38 +0800

    format

--- src/02/07_joining_thread.cpp
+++ src/02/07_joining_thread.cpp
@@ -1,15 +1,15 @@
 #include "07_joining_thread.h"
 
-void do_something(int& i) { i++; }
+void do_something(int* i) { (*i)++; }
 void oops();
 void do_something_in_current_thread() {}
 
 struct func {
-    int& i;
-    func(int& i_): i(i_) {}
+    int* i;
+    explicit func(int* i_): i(i_) {}
     void operator()() {
         for (unsigned j = 0; j < 1000000; j++) {
-            do_something(i);  // 
+            do_something(i);  //
         }
     }
 };
@@ -21,6 +21,6 @@ int main() {
 
 void oops() {
     int some_local_state = 0;
-    joining_thread th(std::thread(func(some_local_state)));
+    joining_thread th(std::thread(func(&some_local_state)));
     do_something_in_current_thread();
-}  
+}

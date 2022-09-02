b004e940ce13e85586941951df6894d743e5f712
consolexin <consolexin@gmail.com>
2022-09-02 16:28:11 +0800

    format

--- src/02/03_thread_guard.cpp
+++ src/02/03_thread_guard.cpp
@@ -1,6 +1,6 @@
 #include <thread>
 
-void do_something(int& i);
+void do_something(int* i);
 void oops();
 void do_something_in_current_thread() {}
 
@@ -13,16 +13,16 @@ public:
             t.join();
         }
     }
-    thread_guard(thread_guard const&)=delete;
-    thread_guard& operator=(thread_guard const&)=delete;
+    thread_guard(thread_guard const&) = delete;
+    thread_guard& operator=(thread_guard const&) = delete;
 };
 
 struct func {
-    int& i;
-    func(int& i_): i(i_) {}
+    int* i;
+    explicit func(int* i_): i(i_) {}
     void operator()() {
         for (unsigned j = 0; j < 1000000; j++) {
-            do_something(i);  // 
+            do_something(i);  //
         }
     }
 };
@@ -32,17 +32,17 @@ int main() {
     return 0;
 }
 
-void do_something(int& i) {
-    i++;
+void do_something(int* i) {
+    (*i)++;
 }
 
 void oops() {
     int some_local_state = 0;
-    func my_func(some_local_state);
+    func my_func(&some_local_state);
     std::thread t(my_func);
     thread_guard g(t);
     do_something_in_current_thread();
-}  
+}
 
 /**
  * @brief 

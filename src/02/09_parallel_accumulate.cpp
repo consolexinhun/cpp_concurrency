30c57b98899474a3c31bfbba5f9d9b5571b541ab
consolexin <consolexin@gmail.com>
2022-09-02 16:40:03 +0800

    format

--- src/02/09_parallel_accumulate.cpp
+++ src/02/09_parallel_accumulate.cpp
@@ -10,8 +10,8 @@ using std::endl;
 
 template<typename Iterator, typename T>
 struct accumulate_block {
-    void operator()(Iterator first, Iterator last, T& result) {
-        result = std::accumulate(first, last, result);
+    void operator()(Iterator first, Iterator last, T* result) {
+        *result = std::accumulate(first, last, *result);
     }
 };
 
@@ -36,25 +36,25 @@ int main() {
 
 template<typename Iterator, typename T>
 T parallel_accumulate(Iterator first, Iterator last, T init) {
-    unsigned long const length = std::distance(first, last);
+    uint64_t const length = std::distance(first, last);
     if (length == 0) {
         return init;
     }
 
-    unsigned long const min_per_thread = 25;
-    unsigned long const max_threads = (length + min_per_thread - 1) / min_per_thread;  // 其实就是向上取整
-    unsigned long const hardware_threads = std::thread::hardware_concurrency();
+    uint64_t const min_per_thread = 25;
+    uint64_t const max_threads = (length + min_per_thread - 1) / min_per_thread;  // 其实就是向上取整
+    uint64_t const hardware_threads = std::thread::hardware_concurrency();
 
-    unsigned long const num_threads = std::min(hardware_threads > 0 ? hardware_threads : 2, max_threads);
-    unsigned long const block_size = length / num_threads;
+    uint64_t const num_threads = std::min(hardware_threads > 0 ? hardware_threads : 2, max_threads);
+    uint64_t const block_size = length / num_threads;
 
     std::vector<T> results(num_threads);
     std::vector<std::thread> threads(num_threads-1);  // 因为还有一个主线程
     Iterator block_start = first;
-    for (unsigned long i = 0; i < num_threads-1; i++) {
+    for (uint64_t i = 0; i < num_threads-1; i++) {
         Iterator block_end = block_start;
         std::advance(block_end, block_size);
-        threads[i] = std::thread(accumulate_block<Iterator, T>(), block_start, block_end, std::ref(results[i]));
+        threads[i] = std::thread(accumulate_block<Iterator, T>(), block_start, block_end, &results[i]);
         block_start = block_end;
     }
     accumulate_block<Iterator, T>()(block_start, last, results[num_threads-1]);  // 剩下还有一部分
